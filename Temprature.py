8489eb4de0374a834de2140c43f14288d6e6b662
Omar Salama <salamaom@sheridancollege.ca>
2025-02-12 16:46:42 +0000

    Indicator of Temprature with a message when is lower than 100 as boiling point or higher than 320 as smoke point

--- Temprature.py
+++ Temprature.py
@@ -4,3 +4,16 @@ import random
 temper = random.randint(1, 400)
 
 print("Temperature:", temper)
+
+# Define temperature values
+boilingPoint = 100
+smokePoint = 320
+
+# Check the temperature conditions using nested if-else
+if temper > boilingPoint:
+    if temper > smokePoint:
+        print("Temperature above smoke point")
+    else:
+        print("Temperature above boiling point")
+else:
+    print("Temperature is not very high")
