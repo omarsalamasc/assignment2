4f201c44d26e6477a4790a9f4914ecb23e8a668a
Omar Salama <salamaom@sheridancollege.ca>
2025-02-13 19:02:42 +0000

    Calculate the sum for square using loop

--- Vectors.py
+++ Vectors.py
@@ -6,3 +6,9 @@ vector = [0, 0, 0]
 for i in range(3):
     vector[i] = int(input("Enter value for " + dimensions[i] + ": "))
     print(i)
+
+# Calculate sum of squares using a for loop
+sum_of_squares = 0
+for i in range(3):
+    sum_of_squares = sum_of_squares + (vector[i] * vector[i])
+    print("Current sum of squares:", sum_of_squares)
