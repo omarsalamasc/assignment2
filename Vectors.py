722d0a19940c10fd82e6d06f8e88557e2286b6a1
Omar Salama <salamaom@sheridancollege.ca>
2025-02-13 19:02:42 +0000

    ask for user input to beging for loop

--- Vectors.py
+++ Vectors.py
@@ -2,4 +2,7 @@
 dimensions = ['x', 'y', 'z']
 vector = [0, 0, 0]
 
-
+# Get user input using a for loop
+for i in range(3):
+    vector[i] = int(input("Enter value for " + dimensions[i] + ": "))
+    print(i)
